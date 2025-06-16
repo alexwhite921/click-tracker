5da27a73b859b37b65f4ddac350d74f7a69f028e
alexwhite921 <alexprudcky@gmail.com>
2025-06-16 15:21:15 +0300

    Update database.py

--- db/database.py
+++ db/database.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-print(f"[DEBUG] DATABASE_URL = {DATABASE_URL}")
+DATABASE_URL = 'postgresql://postgres:qcgZtqenHndhcBlAJWxyxqJeBkKBGOhA@shuttle.proxy.rlwy.net:58997/railway'
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
